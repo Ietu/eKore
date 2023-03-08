373d0ba8b70b3da9f830bf3fb0e1948f808af337
Ietu <54209182+Ietu@users.noreply.github.com>
2023-03-09 01:46:38 +0200

    Add files via upload

--- eKore.py
+++ eKore.py
@@ -10,19 +10,25 @@ import json
 import msvcrt
 import discord
 import sys
-import tools.discord
+from tools.utiltools import utilmenu
+from tools.discordtools import discord
+from tools.filetools import filetool
+from tools.networktools import networkmenu
 from rich.progress import Progress
 from rich import align
 from lolpython import lol_py 
 from tqdm import tqdm
 from colorama import Fore, init, Style
-from tools import discord
+
 
 os.system("cls")
 
 center = shutil.get_terminal_size().columns
 pcname = (os.getenv('COMPUTERNAME'))
 username = os.getlogin()
+
+#pcname = "PCNAME"
+#username = "USERNAME"
 
 init(autoreset=True)
 green = Fore.LIGHTGREEN_EX
@@ -69,7 +75,7 @@ def scrollText(stdscr):
         stdscr.addstr(y, x + i, char, curses.color_pair(1))
         stdscr.refresh()
         time.sleep(0.1)
-    time.sleep(1)
+    time.sleep(1.5)
 
 def gui():
     os.system("cls")
@@ -84,10 +90,11 @@ def gui():
                                  {B}────────────────────────────────────────{w}
                                  {B}╔═══════════════════╦═══════════════════╗{w}
                                  {B}║ {w}[{C}1{w}] Quit          {B}║ {w}[{C}2{w}] Show Sys Info {B}║{w}
+                                 {B}╠═══════════════════╬═══════════════════╣
                                  {B}║ {w}[{C}3{w}] Discord Tools {B}║ {w}[{C}4{w}] File Tools    {B}║{w}
                                  {B}║ {w}[{C}5{w}] Text Editor   {B}║ {w}[{C}6{w}] Network Tools {B}║{w}
-                                 {B}║ {w}[{C}7{w}] Image Tools   {B}║ {w}[{C}8{w}] -             {B}║{w}
-                                 {B}║ {w}[{C}9{w}] Game Launcher {B}║ {w}[{C}10{w}] -            {B}║{w}
+                                 {B}║ {w}[{C}7{w}] Image Tools   {B}║ {w}[{C}8{w}] Util Tools    {B}║{w}
+                                 {B}║ {w}[{C}9{w}] Games         {B}║ {w}[{C}10{w}] -            {B}║{w}
                                  {B}║ {w}[{C}11{w}] -            {B}║ {w}[{C}12{w}] -            {B}║{w}
                                  {B}║ {w}[{C}13{w}] -            {B}║ {w}[{C}14{w}] -            {B}║{w}
                                  {B}╚═══════════════════╩═══════════════════╝{w} 
@@ -101,16 +108,49 @@ def menu():
 {B}╔═{B}[{C}{pcname}{Y}@{C}{username}{B}]
 {B}╚══[{R}OPTION{B}]═> {C}~ {Y}$ {RE}""")
         if x == '1':
+            print(f"{R}Quitting...{RE}")
+            time.sleep(1)
+            os.system("cls")
             exit()
         elif x == '2':
             showInfo()
         elif x == '3':
-            tools.discord.start()
+            discord.start()
         elif x == '4':
-            break
+            filetool.start()
         elif x == '5':
-            break
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
         elif x == '6':
+            networkmenu.start()
+        elif x == '7':
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
+        elif x == '8':
+            utilmenu.start()
+        elif x == '9':
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
+        elif x == '10':
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
+        elif x == '11':
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
+        elif x == '12':
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
+        elif x == '13':
+            print(f"{R}Exiting...")
+            time.sleep(1)
+            exit()
+        elif x == '14':
             print(f"{R}Exiting...")
             time.sleep(1)
             exit()
@@ -122,17 +162,15 @@ def menu():
 
 def showInfo():
     with Progress() as progress:
-        task1 = progress.add_task("[white]Loading information...", total=1000)
-        task2 = progress.add_task("[white]Processing...", total=1000)
+        task1 = progress.add_task(f"{C}> {w}[white]Loading information...", total=1000)
+        task2 = progress.add_task(f"{C}> {w}[white]Processing...", total=1000)
         while not progress.finished:
             progress.update(task1, advance=15)
             progress.update(task2, advance=10)
             time.sleep(0.02)
-    
+    print("\033[K\033[K")
     os.system("neofetch -c BLUE -ac BLUE")
-    print(f"Press '{R}Enter{w}' to go back.")
-
-
+    print(f"{C}> {w}Press '{R}Enter{w}' to go back.")
 
 if __name__ == "__main__":
     start()
