4c369ad23e786d98fa79d5d3c493e205df5e4684
Ietu <54209182+Ietu@users.noreply.github.com>
2023-03-12 15:50:14 +0200

    Delete eKore directory

--- eKore/tools/filetools/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-key = "Hello"
-
-# file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'output'))
-# output_path = os.path.join(file_path, 'output.txt')
-# with open(output_path, 'w') as key_file:
-#     key_file.write(key)
